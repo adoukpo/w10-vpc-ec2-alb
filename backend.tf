05d1bcc443d45711dd9b0fb90f133414f7f31ecf
adoukpo <mawulolosdoukpo@gmail.com>
2024-08-28 03:07:19 -0500

    terraform week10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "w7-ad-terraform" #replace with your bucket
+    bucket         = "astrgfhgnhfmgb" #replace with your bucket
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable" #replace with the bucket
+    dynamodb_table = "fgvncmvhmnmvbc" #replace with the bucket
   }
 }
